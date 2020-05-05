11f2dbcf3717b0e96842c59c375d7b1f6b014270
andremlsantos <andremlsantos@gmail.com>
2020-05-05 19:00:04 +0200

    Updates

---
+++ precache-manifest.df3f86c4ed90db9a00ffb9b55c53dd6d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9821a1a68a4a12f756b851ac76f453d4",
+    "url": "/secfi-front-end/index.html"
+  },
+  {
+    "revision": "afe4ee8568927ba928f7",
+    "url": "/secfi-front-end/static/css/main.3caa289c.chunk.css"
+  },
+  {
+    "revision": "c23e964215a72233fd41",
+    "url": "/secfi-front-end/static/js/2.1834dbf2.chunk.js"
+  },
+  {
+    "revision": "b4cabae32341920cfb4131dbdebda2c2",
+    "url": "/secfi-front-end/static/js/2.1834dbf2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "afe4ee8568927ba928f7",
+    "url": "/secfi-front-end/static/js/main.aec58b0f.chunk.js"
+  },
+  {
+    "revision": "a7c37d76ef7afc3d5ff1",
+    "url": "/secfi-front-end/static/js/runtime-main.607eea75.js"
+  }
+]);
